39d6e28b3341626ea2c066eaf9cdac45f5e0268f
kevindpatino@gmail.com <kevindpatino@gmail.com>
2023-10-22 20:37:39 -0400

    Some bugs in the rotation

--- common/controls.cpp
+++ common/controls.cpp
@@ -19,21 +19,13 @@ glm::mat4 getProjectionMatrix(){
 	return ProjectionMatrix;
 }
 
+float initialFoV = 45.0f;
 
-// Initial position : on +Z
 glm::vec3 position = glm::vec3( 0, 0, 5 );
 glm::vec3 target = glm::vec3(0.0f, 0.0f, 0.0f);
 
-// Initial horizontal angle : toward -Z
 float horizontalAngle = 3.14f;
-// Initial vertical angle : none
 float verticalAngle = 0.0f;
-// Initial Field of View
-float initialFoV = 45.0f;
-
-float speed = 3.0f; // 3 units / second
-float mouseSpeed = 0.005f;
-
 glm::vec3 direction(
 		sin(verticalAngle) * cos(horizontalAngle), 
 		sin(verticalAngle),
@@ -42,13 +34,34 @@ glm::vec3 direction(
 
 glm::vec3 up = glm::vec3(0.0f,1.0f,0.0f);
 
+void updateCameraVectors(glm::vec3& position, glm::vec3& target, glm::vec3& direction, glm::vec3& up) {
+    direction = glm::normalize(target - position);
+    glm::vec3 right = glm::normalize(glm::cross(direction, up));
+    up = glm::cross(right, direction);
+}
+
+void rotateCameraRadially(float angle, glm::vec3& position, glm::vec3& direction, glm::vec3& up, glm::vec3& target) {
+    glm::vec3 currentPosition = position;
+
+    // Calculate new position in spherical coordinates
+    float x = currentPosition.x * cos(angle);
+    float y = currentPosition.y * cos(angle) - currentPosition.z * sin(angle);
+    float z = currentPosition.x * sin(angle) + currentPosition.y * sin(angle) + currentPosition.z * cos(angle);
+
+    position = glm::vec3(x, y, z);
+}
+
 void updateCameraPosition(float angle, glm::vec3& position, glm::vec3& direction) {
     glm::vec3 currentPosition = position;
     float x = currentPosition.x * cos(angle) - currentPosition.z * sin(angle);
     float z = currentPosition.x * sin(angle) + currentPosition.z * cos(angle);
     position = glm::vec3(x, currentPosition.y, z);
-    direction = glm::normalize(target - position);
 }
+
+void moveCameraRadially(float distance, glm::vec3& position, glm::vec3& direction) {
+    position += direction * distance;
+}
+
 
 void computeMatricesFromInputs(){
 
@@ -68,9 +81,23 @@ void computeMatricesFromInputs(){
 		updateCameraPosition(-0.01f, position, direction);
     }
 
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+        moveCameraRadially(-0.1f, position, direction); 
+    }
 
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+        moveCameraRadially(0.1f, position, direction); 
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
+        rotateCameraRadially(0.01f, position, direction,up,target);  // Rotate radially upward
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
+        rotateCameraRadially(-0.01f, position, direction,up,target);  // Rotate radially downward
+    }
+	updateCameraVectors(position,target,direction,up);
 	// Up vector
-
 	float FoV = initialFoV;// - 5 * glfwGetMouseWheel(); // Now GLFW 3 requires setting up a callback for this. It's a bit too complicated for this beginner's tutorial, so it's disabled instead.
 
 	// Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
